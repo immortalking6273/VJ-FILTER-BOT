1c3c215dcecbf3d566479f4ddd93a928b6bfa8c1
immortalking6273 <142087389+immortalking6273@users.noreply.github.com>
2023-12-29 10:10:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
+\r
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
@@ -13,4 +13,4 @@ fi
 cd /KAKASHI-FILTER-BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+
